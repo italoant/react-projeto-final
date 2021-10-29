a08b3f898cc5d635db9d827518dff293a3209035
Camila Silva <silva.camilam27@gmail.com>
2021-10-29 20:56:39 -0300

    Adiciona conteudo do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,5 +9,54 @@ export const Footer = styled.footer`
     width: 100%;
     text-align: center;
     justify-content: center;
-    height: 10vh;
 `
+export const Conteudo = styled.footer`
+    height: 10vh;
+    display: flex; 
+    flex-direction: column;
+    justify-content: center; 
+    align-items: center;
+    color: white;
+
+    .infoFooter{
+        width: 100vw;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+
+        .left{
+            display: flex; flex-direction: column;
+            gap: 10px;
+            padding-left: 2.5rem;
+        }
+
+        .logo{
+            display: flex; align-items: center;
+            gap: 5px; font-weight: bold;
+
+            img{
+                height: 40px;
+            }
+        }
+
+        .right{
+            display: flex; gap: 50px;
+            padding-right: 2.5rem;
+            text-transform: uppercase;
+            text-shadow: .5px .5px rgba(241, 110, 165, 50);
+        }
+    }
+    
+
+    .linha{
+        height: 1px; width: 80vw;
+        background-color: var(--pink3);
+    }
+
+    .copyright{
+        position: absolute;
+        bottom: 0;
+    }
+
+`;
