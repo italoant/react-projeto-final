df8e65690d05b93747452b292cb5a85e52345e7b
italoant <italoant@hotmail.com>
2021-11-02 17:03:38 -0300

    concluir as imagens e adicionei model imcompleto

---
+++ src/pages/Home/Style.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Lista = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 7rem;
+    justify-content: center;
+
+    img {
+        border-radius: 5%;
+        border: 1px solid black;
+        height: 20rem;
+    }
+
+`
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 90vw;
+    margin: 0 auto;
+
+    h2{
+        margin: 2.5%;
+    }
+
+    .imgNoticia{
+        height: 20rem;
+        width: 30rem;   
+        border-radius: 5%;
+        border: 1px solid black;
+    }
+    .noticiasDiv{
+        width: 70vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .noticiasDiv p {
+        margin-bottom: 15%;
+        word-wrap: break-word;
+    }
+    .noticias {
+        padding: 2.5%;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+        margin-bottom: 10vh;
+    }
+`
