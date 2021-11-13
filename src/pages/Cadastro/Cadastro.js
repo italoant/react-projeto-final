f5753aee27d9972e4d6806a08b94e4497d99fb0d
Camila Silva <silva.camilam27@gmail.com>
2021-11-12 23:08:50 -0300

    Adicionando componentes noticias

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -7,6 +7,7 @@ import { useTheme } from "../../context/Theme";
 
 export default function Cadastro(){
 
+    const { themePage } = useTheme()
     const [nome, setNome] = useState ("");
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
