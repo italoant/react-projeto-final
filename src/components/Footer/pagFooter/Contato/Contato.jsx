d8523b7f11f77826d635d7d984583f222123e28e
thayzasantana <thayzasantana3012@gmail.com>
2021-11-13 01:41:43 -0300

    ajustando responsividade da tela de contato

--- src/components/Footer/pagFooter/Contato/Contato.jsx
+++ src/components/Footer/pagFooter/Contato/Contato.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { H1Style, Modal } from '../../../Components';
-import { ContainerForm, Formulario, Botao } from '../../../../pages/Login/Style';
+import { Container, Form, Button } from './StylePag';
 import { useTheme } from "../../../../context/Theme";
 
 
@@ -9,47 +9,42 @@ export default function Contato() {
     const { themePage } = useTheme();
 
     return (
-        <ContainerForm theme={themePage}>
+        <Container theme={themePage}>
             <H1Style> Entre em contato </H1Style>
-                    <Formulario>
-                        <div className="containerForm">
-                            <h4> Preencha os campos deste formulário para entrar em contato com nossa equipe de desenvolvimento Front-End do site PanimeE. </h4>
-                            <form className="formulario">
-                                <div className="campos-form">
-                                    <input type="text" id="nome" placeholder="Nome" />
-                                </div>
-                                
-                                <div className="campos-form">
-                                    <input type="email" placeholder="Email@email.com" id="email" />
-                                </div>
-                                <div className="campos-form">
-                                    <label htmlFor="opcao">Selecione um tema:</label>
-                                        <select className="opcoes" name="opcao" id="opcao">
-                                            <option value="" selected  className="opcao">-</option>
-                                            <optgroup label="Senha:" className="opcao">
-                                                <option value="esqueceuSenha" className="opcao">Esqueceu a senha</option>
-                                            </optgroup>
-                                            <optgroup label="Gerenciamento de conta:" className="opcao">
-                                                <option value="gerenciamento" className="opcao">Solicitação de dados, recuperar conta e exclusão de conta</option>
-                                            </optgroup>
-                                            <optgroup label="Problemas técnicos:" className="opcao">
-                                                <option value="problemasTecnicos" className="opcao">Lag, fechamento</option>
-                                            </optgroup>
-                                            <optgroup label="Outros:" className="opcao">
-                                                <option value="outros" className="opcao">Outros temas</option>
-                                            </optgroup>
-                                        </select>
-                                </div>
-                                <div className="campos-form">
-                                    <label htmlFor="comentario">Comentário: </label>
-                                    <textarea name="comentario" id="comentario" className="coments" placeholder="Nos informe o motivo do contato..." cols="30" rows="5" className="comentario">    
-                                    </textarea>
-                                </div>
-                                <Botao color="#FE6688" type="button"> Cancelar </Botao>
-                                <Botao color="#F16EA5"> Enviar</Botao>
-                            </form>
-                        </div>
-                    </Formulario>
+                    <Form>
+                        <h4> Preencha os campos deste formulário para entrar em contato com nossa equipe de desenvolvimento Front-End do site PanimeE. </h4>
+                            <div className="campos-form">
+                                <input type="text" id="nome" placeholder="Nome" />
+                            </div> 
+                            <div className="campos-form">
+                                <input type="email" placeholder="Email@email.com" id="email" />
+                            </div>
+                            <div className="campos-form">
+                                <label htmlFor="opcao">Selecione um tema:</label>
+                                    <select className="opcoes" name="opcao" id="opcao">
+                                        <option value="" selected  className="opcao">-</option>
+                                        <optgroup label="Senha:" className="opcao">
+                                            <option value="esqueceuSenha" className="opcao">Esqueceu a senha</option>
+                                        </optgroup>
+                                        <optgroup label="Gerenciamento de conta:" className="opcao">
+                                            <option value="gerenciamento" className="opcao">Solicitação de dados, recuperar conta e exclusão de conta</option>
+                                        </optgroup>
+                                        <optgroup label="Problemas técnicos:" className="opcao">
+                                            <option value="problemasTecnicos" className="opcao">Lag, fechamento</option>
+                                        </optgroup>
+                                        <optgroup label="Outros:" className="opcao">
+                                            <option value="outros" className="opcao">Outros temas</option>
+                                        </optgroup>
+                                    </select>
+                            </div>
+                            <div className="campos-form">
+                                <label htmlFor="comentario">Comentário: </label>
+                                <textarea name="comentario" id="comentario" className="coments" placeholder="Nos informe o motivo do contato..." cols="30" rows="5" className="comentario">    
+                                </textarea>
+                            </div>
+                            <Button color="#FE6688" type="button"> Cancelar </Button>
+                            <Button color="#F16EA5"> Enviar</Button>
+                    </Form>
                     <Modal color="#FE6688" 
                         nomeBotao="teste" 
                         img="https://pa1.narvii.com/6595/33963d95eb637c3e8cbed27e7a204ae821723c2c_hq.gif" 
@@ -57,6 +52,6 @@ export default function Contato() {
                         mensagem="Agradecemos!"
                         mensagem2="ありがとう"
                     /> 
-        </ContainerForm>
+        </Container>
     );
 }
--- src/components/Footer/pagFooter/Contato/StylePag.js
+++ src/components/Footer/pagFooter/Contato/StylePag.js
@@ -1,163 +1,270 @@
 import styled from "styled-components";
 
-export const EstiloContato = styled.div`
-    width: 100vw; 
-    height: 100vh;
+
+export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    text-shadow: 1px 1px 10px white;
-    margin: 8rem 0;
-    
-    h4{
-        flex-wrap: wrap;
-        text-align: center;
-        color: rgba(0, 0, 0, 0.65);
+    background: ${props => props.theme.backgroundSecondary};
+    height: 150vh; 
+
+    @media (max-width: 1326px){
+        height: 160vh;
     }
 
-    .containerForm{
-        width: 30vw;
+    @media (max-width: 1024px){
+        height: 165vh;
     }
 
-    .formulario{
+    @media (max-width: 994px){
+        height: 170vh;
+    }
+
+    @media (max-width: 600px){
+        height: 185vh;
+    }
+
+    @media (max-width: 400px){
+        height: 190vh;
+    }
+
+    @media (max-width: 375px){
+        height: 210vh;
+    }
+
+    @media (max-width: 350px){
+        height: 250vh;
+    }
+
+`
+
+export const Form = styled.form`
+    background-color: #FFBDBF;
+    width: 38vw;
+    text-align: center;
+    border-radius: 20px;
+    padding: 7vh;
+    box-shadow: 1px 1px rgba(0, 0, 0, 25%);
+
+    label{
+        font-size: 1.4rem;
+        margin-bottom: 1.5vh;
+        color: #FFFFFF;
+        text-shadow: .5px .5px rgba(241, 110, 165, 50);
+    }
+
+    .campos-form{
+        margin-top: 4.8vh;
+        margin-bottom: 0.5vh;
         display: flex;
         flex-direction: column;
-        gap: 1rem;
-        margin-top: 2rem;
-
-        label{
-            text-align: left;
-            font-size: 1rem;
-        }
-
-        input::placeholder, textarea::placeholder{
-            color: rgba(0, 0, 0, 0.50);
-        }
-
-        .opcoes, input, textarea{
-            background: none; 
-            border: none; 
-            outline: none;
-            border-radius: 5px;
-            background-color: rgba(255, 255, 255, 0.5);
-            padding: 1%;
-            box-shadow: 1px 1px 5px rgba(0, 0, 0, 25%);
-        }
-
-        #nome, #email, .opcoes{
-            height: 4vh;
-        }
-
-        .opcao{
-            border: none;
-            box-shadow: 1px 1px rgba(0, 0, 0, 25%);
-            background: #EED9D6;
-            color: rgba(0, 0, 0, 0.75);
-            padding: 2%;
-        }
-
-        select{
-            text-shadow: 1px 1px 10px white;
-        }
-        
-        select option:checked {
-            background: var(--pink2);
-        }
-       
-
-        textarea{
-            box-shadow: 3px 3px 10px rgba(40, 0, 5, 0.30);
-        }
-        
-        input{
-            box-shadow: 3px 3px 10px rgba(40, 0, 5, 0.35);
-        }
-        
-    }
-
-    .botoes{
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-        
-        button{
-            width: 40%;
-            height: 2rem;
-            border-radius: 25px;
-            border: none; outline: none;
-            color: white;
-            text-transform: uppercase;
-            font-weight: bold;
-        }
-
-        .btnCancelar{
-            background-color: #FE6688;
-        }
-
-        .btnEnviar{
-            background-color: #F16EA5;
-        }
-    }
-    
-    #popup{
-        width: 100vw; height: 100v;
-        background-color: rgba(0, 0, 0, 0.50);
-        position: fixed;
-        top: 0; left: 0;bottom: 0;
-        display: none;
-        flex-direction: column;
-        justify-content: center;
         align-items: center;
     }
 
-    .gif{
-        display: flex;
-        justify-content: center;
-        margin-top: 5rem;
-    }
-    img{
-        border: 1px solid black;
-        padding: 1rem;
-        border-radius: 3px;
-    }
-
-    @media (max-width:700px){
-        margin: 3rem 0;
-        .containerForm{
-            width: 60vw;
-        }
-
-        h4{
-            font-size: 1rem;
-            text-shadow: 1px 1px 5px white;
-        }
-
-        .formulario{
-            margin: 0%;
-        }
-
-        button{
-            font-size: 0.7rem;
-            font-weight: bold;
-        }
+    .campos-form input{
+        padding: 2.5%;
+        width: 25vw;
+        min-height: 6vh;
+        border: none;
+        border-radius: 10px;
+        background-color: #FFFFFF;
+        color: #A9A9A9;
+        outline: none;
     }
 
-    @media (max-width:435px){
-        margin: 2rem 0;
-        .botoes{
-            justify-content: space-around;
+    .campos-form select{
+        width: 25vw;
+        min-height: 6vh;
+        border: none;
+        border-radius: 10px;
+    }
 
-            button{
-                width: 5rem;
-                font-size: 0.5rem;
-                font-weight: bold;
+    .campos-form textarea{
+        width: 25vw;
+        padding: 2.5%;
+        border: none;
+        border-radius: 10px;
+    }
+
+    .campos-form p{
+       text-align: center;
+    }
+
+    //Responsividade do formulário
+
+    @media (max-width: 1326px){
+        width: 50%;
+
+        .campos-form input{
+            width: 33vw;
+        }
+
+        .campos-form select{
+            width: 33vw;
+        }
+
+        .campos-form textarea{
+            width: 33vw;
+        }
+    }
+
+    @media (max-width: 994px){
+            width: 51%;
+
+            .mensagem{
+                font-size: 1.1rem;
             }
+    }
+
+    @media (max-width: 982px){
+        width: 55%;
+
+        h1{
+            font-size: 3rem;
+        }
+
+        .campos-form input{
+            width: 36vw;
+        }
+
+        .campos-form select{
+            width: 36vw;
+        }
+
+        .campos-form textarea{
+            width: 36vw;
         }
     }
 
-    @media (max-width:280px){
-        margin: 5rem 0;
+    @media (max-width: 862px){
+        width: 60%;
+
+        .campos-form input{
+            width: 39vw;
+        }
+
+        .campos-form select{
+            width: 39vw;
+        }
+
+        .campos-form textarea{
+            width: 39vw;
+        }
+
+        .mensagem{
+                font-size: 1rem;
+            }
     }
-`;
+
+    @media (max-width: 740px){
+        padding-left: 0;
+        padding-right: 0;  
+        
+        .mensagem{
+                font-size: 0.9rem;
+            }
+
+            h4{
+                padding: 2.5%;
+            }
+    }
+
+    @media (max-width: 700px){
+        width: 70%;
+
+        .campos-form input{
+            width: 50vw;
+        }
+
+        .campos-form select{
+            width: 50vw;
+        }
+
+        .campos-form textarea{
+            width: 50vw;
+        }
+    }
+
+    @media (max-width: 550px){
+        width: 90%;
+
+        .campos-form input{
+            min-width: 65vw;
+        }
+
+        .campos-form select{
+            width: 65vw;
+        }
+
+        .campos-form textarea{
+            width: 65vw;
+        }
+    }
+    
+    @media (max-width: 320px){
+
+        h1{
+            font-size: 2rem;
+        }
+        
+    }
+`
+
+export const Button = styled.button`
+        margin: 2vh;
+        width: 11.5vw;
+        border-radius: 10px;
+        border: none;
+        color: #FFFFFF;
+        height: 6vh;
+        font-size: 1.1rem;
+        background-color: ${props => props.color};
+        
+        :hover{
+            color: #A9A9A9;
+            background-color: #EED9D6;
+        }
+
+        // Responsividade dos botões
+    
+        @media (max-width: 1326px){
+            width: 15.1vw;
+        }
+
+        @media (max-width: 982px){
+            width: 16vw;
+        }
+
+        @media (max-width: 862px){
+            width: 17.5vw;
+        }
+        
+        @media (max-width: 840px){
+            width: 39.5vw;
+            height: 7.5vh;
+        }
+
+        @media (max-width: 768px){
+
+            margin-left: 2vw;
+        }
+
+        @media (max-width: 740px){
+            width: 39vw;
+            margin-left: 2vw;
+        }
+
+        @media (max-width: 700px){
+            width: 50vw; 
+        }
+
+        @media (max-width: 550px){
+            width: 66vw;
+            margin-left: 3vw;
+        }
+
+        @media (max-width: 375px){
+            margin-left: 4vw;
+        }
+`
