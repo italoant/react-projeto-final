af77299fa114564fdb12b41d5631094b828f19a5
camilams27 <silva.camilam27@gmail.com>
2021-11-14 23:26:40 -0300

    Ajustando tela noticias

--- src/pages/Noticias/Noticias.js
+++ src/pages/Noticias/Noticias.js
@@ -27,13 +27,6 @@ export default function Noticias(){
                         dataL="06/11/2021"
                         resumoL="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at gravida ipsum, quis tristique libero. Aenean lobortis nisi consequat, luctus dolor non, sodales erat. Aenean congue turpis sed neque malesuada laoreet. Sed consequat nibh a aliquet faucibus. "
                     />
-                     <NoticiasL 
-                        imgL="https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt8739d07baed7d77d/5ed9b1c197379739c07664d9/Arcane_Announcement_Banner.jpg"
-                        nomeL="Nome"
-                        statusL="Lançamento"
-                        dataL="06/11/2021"
-                        resumoL="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at gravida ipsum, quis tristique libero. Aenean lobortis nisi consequat, luctus dolor non, sodales erat. Aenean congue turpis sed neque malesuada laoreet. Sed consequat nibh a aliquet faucibus. "
-                    />
                 </div>
                <div className="right">
                 <h2> Em alta</h2>
